2bec42e616a1d1d929ff50b2643ec700640da9aa
carhernandez <carlos.hernandez@convertic.com.co>
2022-12-27 20:35:13 -0500

    Initial commit

---
+++ src/main/java/com/platzi/apicompras/ApicomprasApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.apicompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicomprasApplication.class, args);
+	}
+
+}
